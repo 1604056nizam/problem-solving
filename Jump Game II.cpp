153df5b265706edb8ea5225f20dc96cb0772520b
Md. Nizam Uddin 1604056 <nu292257@gmail.com>
2023-05-07 18:45:03 +0600

    https://leetcode.com/problems/jump-game-ii/

---
+++ Jump Game II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int farthest = 0;
+        int jumps = 0;
+        int current = 0;
+
+        for(int i = 0; i< nums.size()-1; i++){
+            farthest = max(farthest, nums[i]+i);
+
+            if(i == current){
+                jumps++;
+                current = farthest;
+            }
+        }
+        return jumps;
+        
+    }
+};
