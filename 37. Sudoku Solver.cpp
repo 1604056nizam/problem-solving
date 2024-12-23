c2586d94e91966f5bd4b3a12fbfe6f0f65ed520f
nizam <nu292257@gmail.com>
2024-12-24 01:00:43 +0600

    37. Sudoku Solver

---
+++ 37. Sudoku Solver.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+
+        for (int i = 0; i < 9; i++) {
+            if (i != row && board[i][col] == c) {
+                return false;
+            }
+
+            if (i != col && board[row][i] == c) {
+                return false;
+            }
+
+            if ((3 * (row / 3) + i / 3) != row && (3 * (col /3) + i % 3) != col  &&
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool generateSudoko(vector<vector<char>>& board) {
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (generateSudoko(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) { generateSudoko(board); }
+};
